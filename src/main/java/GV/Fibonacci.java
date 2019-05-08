6e38e3163032601f7b0fd1127c30e5cad6e273d1
zloystarosta <zloystarosta@gmail.com>
2019-05-08 20:25:23 +0200

    fibonacci

--- src/main/java/GV/Fibonacci.java
+++ src/main/java/GV/Fibonacci.java
@@ -6,12 +6,29 @@ import java.util.List;
 
 public class Fibonacci {
     String outputdata;
-    List<BigInteger> list;
     BigInteger number;
+
+    public void setNumber(BigInteger number) {
+        this.number = number;
+    }
 
     public Fibonacci(String inputdata) {
         number = new BigInteger(inputdata);
-        list = new ArrayList<BigInteger>();
+    }
+
+    public String createRezult(){
         outputdata = "";
+        for(int i=1;number.compareTo(BigInteger.valueOf(i))>-1;i++){
+            BigInteger help = findNumeric(BigInteger.valueOf(i));
+            outputdata+=help.toString();
+            outputdata+=" ";
+        }
+        return outputdata;
+    }
+
+    BigInteger findNumeric(BigInteger n) {
+        if (n.compareTo(BigInteger.valueOf(1))==0) return BigInteger.valueOf(1);
+        if (n.compareTo(BigInteger.valueOf(2))==0) return BigInteger.valueOf(1);
+        return findNumeric(n.subtract(BigInteger.valueOf(1))).add(findNumeric(n.subtract(BigInteger.valueOf(2))));
     }
 }
