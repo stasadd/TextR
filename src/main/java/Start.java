79fe5b76fd8ab19498b1c289d9f7982b689469b7
TD-user <td87td87@gmail.com>
2019-05-08 19:39:37 +0300

    start

---
+++ src/main/java/Start.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Start extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/startFormFXML.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setWidth(1200);
+        primaryStage.setHeight(800);
+        primaryStage.setTitle("Notepad--");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
